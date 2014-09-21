2e969ecf6ed8c7eb888f4cd1531396579b2cf7c9
matt <mgwidmann@gmail.com>
2014-09-20 21:22:47 -0400

    Link database until can get a real dbms system up and running

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,13 +20,13 @@ set :deploy_to, '/var/www/appointments'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml db/staging.sqlite3}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
