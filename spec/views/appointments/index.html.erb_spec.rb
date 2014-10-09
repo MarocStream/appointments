9f8dc17f029ede626da422de6b84424bf3ff7fe9
matt <mgwidmann@gmail.com>
2014-10-08 23:45:30 -0400

    And fix tests...

--- spec/views/appointments/index.html.erb_spec.rb
+++ spec/views/appointments/index.html.erb_spec.rb
@@ -4,12 +4,14 @@ describe "appointments/index" do
   before(:each) do
     assign(:appointments, [
       stub_model(Appointment,
-        :user_id => 1,
-        :appointment_type_id => 2
+        :user => stub_model(User, display: "USER"),
+        :appointment_type => stub_model(AppointmentType, name: "TYPE"),
+        :start => 1.day.ago
       ),
       stub_model(Appointment,
-        :user_id => 1,
-        :appointment_type_id => 2
+        :user => stub_model(User, display: "USER"),
+        :appointment_type => stub_model(AppointmentType, name: "TYPE"),
+        :start => 2.days.ago
       )
     ])
   end
@@ -17,7 +19,7 @@ describe "appointments/index" do
   it "renders a list of appointments" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "USER", :count => 2
+    assert_select "tr>td", :text => "TYPE", :count => 2
   end
 end
--- spec/views/appointments/show.html.erb_spec.rb
+++ spec/views/appointments/show.html.erb_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe "appointments/show" do
   before(:each) do
     @appointment = assign(:appointment, stub_model(Appointment,
-      :user_id => 1,
-      :appointment_type_id => 2
+      :user => stub_model(User, display: "USER"),
+      :appointment_type => stub_model(AppointmentType, name: "TYPE")
     ))
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    rendered.should match(/2/)
+    rendered.should match(/USER/)
+    rendered.should match(/TYPE/)
   end
 end
