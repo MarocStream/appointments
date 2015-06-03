0efef9f2fb97a67b4fc0213947ae34bc022e3b6b
matt <mgwidmann@gmail.com>
2015-06-02 23:31:52 -0400

    Move phoenix back to /ws because nginx likes it better that way :confused:

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.phoenix_url = nil # Will default to current host:port
-  config.phoenix_path = '/p/ws'
+  config.phoenix_path = '/ws'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
