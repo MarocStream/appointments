2f842169afc4666a7b74daee55b355392e48aa8f
matt <mgwidmann@gmail.com>
2015-06-02 22:18:59 -0400

    Get ws url fixed correctly...

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Appointments
   class Application < Rails::Application
 
-    config.phoenix_url = '/'
+    config.phoenix_url = nil
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
