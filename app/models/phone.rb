9879f3c1ea564d5d8e0e84862f225033093efae4
matt <mgwidmann@gmail.com>
2016-03-28 23:13:39 -0400

    Rename home to evening and work to daytime and make daytime default.

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,7 +2,7 @@ class Phone < ActiveRecord::Base
 
   belongs_to :user
 
-  enum kind: {home: 0, work: 1, cell: 2}
+  enum kind: {daytime: 0, evening: 1, cell: 2}
 
   validates :kind, :number, presence: true
 
